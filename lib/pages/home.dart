70af65749db255f36300f1ce2ccb16febb1449ad
Keith Robertson <keith@openhome.org>
2020-01-18 22:18:35 +0000

    Text overlay and buttons working

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,26 +2,70 @@ import 'package:flutter/material.dart';
 
 import './page.dart';
 
+final String coverString =
+    "One week of world class flute tuition in Perthshire, Scotland Dates: 26th July - 1st August, 2020"
+        .toUpperCase();
+
 class Home extends Page {
   const Home(String title) : super(title);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ListView(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Image.asset(
-                  'assets/strathallan.jpg',
-                  fit: BoxFit.fitWidth,
-                ),
-              ),
-            ],
+    return ListView(
+      children: [
+        Container(
+          constraints: BoxConstraints(minHeight: 700.0),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/strathallan.jpg'),
+              fit: BoxFit.cover,
+            ),
           ),
-        ],
-      ),
+          child: Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  coverString,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 55.0,
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                Padding(padding: EdgeInsets.symmetric(vertical: 30.0)),
+                ButtonBar(
+                  alignment: MainAxisAlignment.center,
+                  buttonMinWidth: 180.0,
+                  buttonHeight: 50.0,
+                  buttonPadding: EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                    vertical: 16.0,
+                  ),
+                  children: [
+                    RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0)),
+                      child: Text("Book Now".toUpperCase()),
+                      color: Colors.red,
+                      onPressed: () => {},
+                    ),
+                    RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0)),
+                      child: Text("Auditor Tickets".toUpperCase()),
+                      color: Colors.green,
+                      onPressed: () => {},
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
